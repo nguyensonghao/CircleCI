641ce187a65c6705273cb9b601d96baab98c5d66
nguyensonghao94 <hao.nguyensong@powergatesoftware.com>
2019-12-26 13:39:45 +0700

    Fix bug

--- test/auth/filterPortal/setupSettings.test.js
+++
@@ -1,9 +0,0 @@
-const config = require('config');
-const assert = require('assert');
-const { Builder, By, until } = require('selenium-webdriver');
-
-const UtilHelper = require('../../helpers/util.helper');
-
-describe('/Change the logic of filter', () => {
-
-})
